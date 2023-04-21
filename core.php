cb2be5ed0f0a621ee8bc76c17fd3b8b0df8fe7ba
Volkan Küçükbudak <VolkanSah@users.noreply.github.com>
2023-04-21 14:37:02 +0200

    Update core.php

--- core.php
+++ core.php
@@ -26,6 +26,20 @@ add_action('admin_menu', 'wpwm_openai_settings');
 
 function wpwm_openai_settings_page() {
     $openai_api_key = get_option('openai_api_key');
+    $model = get_option('model');
+    $temperature = get_option('temperature');
+    $max_tokens = get_option('max_tokens');
+    $top_p = get_option('top_p');
+    $frequency_penalty = get_option('frequency_penalty');
+    $presence_penalty = get_option('presence_penalty');
+    
+    
+    
+    
+    
+    
+    
+    
     ?>
     <div class="wrap">
         <h1>WPWM OpenAI Comment Reply Settings</h1>
@@ -38,6 +52,38 @@ function wpwm_openai_settings_page() {
                     <td>
                         <input type="password" required name="openai_api_key" class="regular-text" value="<?php echo esc_attr($openai_api_key); ?>" /></td>
                 </tr>
+                        <tr valign="top">
+            <th scope="row">Model</th>
+            <td>
+                <select name="model">
+                    <option <?php selected($model, 'text-davinci-003'); ?> value="text-davinci-003">text-davinci-003</option>
+                    <!-- Add other models here -->
+                </select>
+            </td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Temperature</th>
+            <td><input type="number" step="0.01" min="0" max="1" name="temperature" value="<?php echo esc_attr($temperature); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Max Tokens</th>
+            <td><input type="number" min="1" name="max_tokens" value="<?php echo esc_attr($max_tokens); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Top P</th>
+            <td><input type="number" step="0.01" min="0" max="1" name="top_p" value="<?php echo esc_attr($top_p); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Frequency Penalty</th>
+            <td><input type="number" step="0.01" min="0" max="1" name="frequency_penalty" value="<?php echo esc_attr($frequency_penalty); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Presence Penalty</th>
+            <td><input type="number" step="0.01" min="0" max="1" name="presence_penalty" value="<?php echo esc_attr($presence_penalty); ?>" /></td>
+        </tr>
+    </table>
+    <?php
+}
             </table>
             <?php submit_button(); ?>
         </form>
@@ -56,6 +102,13 @@ add_filter('comment_row_actions', 'wpwm_openai_add_button_to_comment_row_actions
 // Adds JavaScript code to process OpenAI responses
 function wpwm_openai_add_js_to_comment_page() {
     $openai_api_key = get_option('openai_api_key');
+    $model = get_option('model');
+    $temperature = get_option('temperature');
+    $max_tokens = get_option('max_tokens');
+    $top_p = get_option('top_p');
+    $frequency_penalty = get_option('frequency_penalty');
+    $presence_penalty = get_option('presence_penalty');
+    
     ?>
     <script>
         jQuery(document).ready(function ($) {
@@ -81,10 +134,14 @@ function wpwm_openai_add_js_to_comment_page() {
                         "Authorization": "Bearer " + apiKey
                     },
                     data: JSON.stringify({
-                        "model": "text-davinci-003",
-                        "prompt": 'Reply to comment: ' + comment_text,
-                        "max_tokens": 1024,
-                        "temperature": 0.5
+                    "model": "<?php echo esc_attr($model); ?>",
+                    "prompt": 'Reply to comment: ' + comment_text,
+                    "max_tokens": <?php echo esc_attr($max_tokens); ?>,
+                    "temperature": <?php echo esc_attr($temperature); ?>,
+                    "top_p": <?php echo esc_attr($top_p); ?>,
+                    "frequency_penalty": <?php echo esc_attr($frequency_penalty); ?>,
+                    "presence_penalty": <?php echo esc_attr($presence_penalty); ?>
+                
                     }),
                     success: function (response) {
                         var choices = response.choices;
